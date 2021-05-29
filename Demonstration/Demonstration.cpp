04a1e004568df4e1c3666eb21b6056e2a3ace1e8
Reinis <rlukins123@gmail.com>
2021-05-29 21:25:17 +0300

    Test commit

--- Demonstration/Demonstration.cpp
+++ Demonstration/Demonstration.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
+#include "Demonstration.h"
 
 using namespace std;
 
 int main()
 {
-    int number = 5;
+    NewFunction();
     string text = "Hello World!";
     cout << text << endl;
     cout << "Next commit" << endl;
+}
+
+void NewFunction()
+{
+    int number = 5;
+    int number2 = 10;
+    cout << number + number2 << endl;
 }
